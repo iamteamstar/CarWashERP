c903f9e0a5b42c8933c52ccbaf8c722b77839d34
iamteamstar <azadkocak29@gmail.com>
2026-06-21 20:33:28 +0300

    Arac yikama durumlari ve paket turu enum olarak belirlendi.

---
+++ CarWashERP.Domain/Enums/WashStatus.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarWashERP.Domain.Enums
+{
+	public enum WashStatus
+	{
+		Pending = 1,       // Araç teslim alındı, sırada
+		InProgress = 2,    // Yıkama aşamasında
+		Finishing = 3,     // Son dokunuşlar / 5 dk içinde hazır
+		Completed = 4,     // Teslim edildi / Tamamlandı
+		Cancelled = 5      // İptal edildi
+	}
+
+	public enum PackageType
+	{
+		OnlyExterior = 1,  // Sadece Dış
+		InteriorAndExterior = 2 // İç - Dış
+	}
+}
