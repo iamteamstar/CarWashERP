a22a9f9aea2ada39d99e7a2406d855651ad592d0
iamteamstar <azadkocak29@gmail.com>
2026-06-21 23:15:27 +0300

    feat(Expense): aylik cirodan dusulecek maliyetler belirlendi.

---
+++ CarWashERP.Domain/Entities/Expense.cs
@@ -0,0 +1,20 @@
+﻿	using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarWashERP.Domain.Common;
+namespace CarWashERP.Domain.Entities
+{
+	public class Expense : BaseEntity, IMustHaveTenant
+	{
+		public int TenantId { get; set; }
+		public string Title { get; set; } = string.Empty; // Örn: Deterjan Alımı, Elektrik Faturası
+		public string Description { get; set; } = string.Empty;
+		public decimal Amount { get; set; }
+		public DateTime ExpenseDate { get; set; }
+
+		// Navigation Properties
+		public Tenant Tenant { get; set; } = null!;
+	}
+}
